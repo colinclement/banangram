48954767a266c331ff1a51461f680388a9457e2d
Colin Clement <colin.clement@gmail.com>
2017-01-15 02:16:33 -0500

    implemented dawg, tested for small lexicon. Finding maximum depths appears to be bottleneck

--- code/graph.py
+++ code/graph.py
@@ -7,24 +7,25 @@ date: 2016-01-14
 Given a lexicon of words, Trie object computes the corresponding trie.
 TODO: Create function or class DAWG which merges suffixes in Trie to
 get the DAWG of a lexicon.
-
 """
 
 
-import string
 from collections import defaultdict
+#import numpy as np
+
+whitespace = '\t\n\x0b\x0c\r ' 
 
 
 class Node(object):
-    def __init__(self, label):
+    def __init__(self):
         """
         input : int, unique label for node
         """
-        assert type(label) is int, 'label must be int'
-        self.label = label
-        self.children = defaultdict(set)
-        self.parents = defaultdict(set)
+        self.children = {}
+        self.parent = None
+        self.parentlabel = None
         self.strset = set([])  # list of complete words
+        self.maxdepth = -1  # not yet calculated
 
     def addchild(self, edge, node):
         """
@@ -37,7 +38,8 @@ class Node(object):
         assert type(node) is Node, 'node must be a Node'
         assert type(edge) is str, 'edge must be a char'
         self.children[edge] = node
-        node.parents[edge] = self
+        node.parent = self
+        node.parentlabel = edge
         return node
 
     def __getitem__(self, c):
@@ -45,7 +47,8 @@ class Node(object):
 
     def __str__(self):
         """ Make print(self) useful """
-        pstr = "{label} \033[1;35;49m("  # pretty purple
+        #pstr = "{label} \033[1;35;49m("  # pretty purple
+        pstr = "\033[1;35;49m("  # pretty purple
         if self.strset:
             pstr += ('{}, '*len(self.strset)).format(*self.strset)[:-2]
         pstr += ")\033[0m: "
@@ -62,32 +65,54 @@ class Node(object):
         edges, that is the next possible characters in the graph. This
         will improve the speed of comparing two nodes when building a DAWG
         """
-        return hash(''.join(sorted(self.children.keys())))
+        eow = str(1*bool(self.strset))
+        return hash(eow + ''.join(sorted(self.children.keys())))
 
 
-class Graph(object):
+class DirectedGraph(object):
     """
     Edges have a char associated with them and point between two nodes
     Nodes are labeled by a unique int, contain a set of strings which are
     complete words
     """
     def __init__(self):
-        self.nodes = [Node(0)]  # First node is empty string
+        self.top = Node()
         self.edges = defaultdict(set)
+        self.maxdepth = -1
 
     @property
-    def top(self):
-        return self.nodes[0]
+    def E(self):
+        """ Number of edges """
+        visited = set([self.top])
+        c = self.top.children.values()
+        E = len(c)
+        queue = set(c)
+        while queue:
+            print(E, queue)
+            n = queue.pop()
+            visited.update(set([n]))
+            c = n.children.values()
+            E += len(c)
+            queue.update(set(c))
+        return E, visited
 
+    @property
+    def nodes(self):
+        """ Return a list of all nodes """
+        def children(node):
+            c = node.children
+            return [node] + reduce(lambda x, y: x+y, 
+                                   [children(c[k]) for k in c], [])
+        return set(children(self.top))  # only unique nodes!
+    
     @property
     def N(self):
         """ Number of nodes """
         return len(self.nodes)
 
     @property
-    def E(self):
-        """ Number of edges """
-        return sum([len(self.edges[c]) for c in self.edges])
+    def leaves(self):
+        return set([n for n in self.nodes if not n.children])
 
     def parselex(self, w):
         """
@@ -99,19 +124,21 @@ class Graph(object):
         assert type(w) == str, "w must be str"
         loc = self.top
         path = ''
-        for c in w:
+        N = len(w)
+        for i, c in enumerate(w):
             if c in loc.children:
                 loc = loc[c]
                 path += c
-            elif c in string.whitespace:
+            elif c in whitespace:
                 loc.strset.add(path)
                 loc = self.top  # end of word, go to top
                 path = ''
             else:  # Create edge, node
-                loc = loc.addchild(c, Node(self.N+1))
-                self.nodes.append(loc)
-                self.edges[c].add((loc.parents[c], loc))
+                loc = loc.addchild(c, Node())
                 path += c
+                if i == N - 1:  # in case last char isnt whitespace
+                    loc.strset.add(path)
+        #self.update_maxdepth()
 
     def _down(self, node, word):
         """ Traverse tree to find the node at which word lives """
@@ -127,9 +154,49 @@ class Graph(object):
         """ Return node corresponding to word or None """
         return self._down(self.top, word)
 
+    #TODO: Consider an iterative instead of recursive implementation
+    def _count_maxdepths(self, node, depth = 0):
+        if node.maxdepth < depth:  # Cut off search if longer path exists
+            node.maxdepth = depth
+            p = node.parent
+            if p:
+                return self._count_maxdepths(p, depth + 1)
+
+    def update_maxdepth(self):
+        """
+        Calculates the maximum depth to leaves for each node.
+        Useful for efficiently trimming Trie into a DAWG
+        """
+        for l in self.leaves:
+            self._count_maxdepths(l)
+
+
+def trie_to_dawg(G):
+        """
+        Merges all identical subtrees in graph G to for a Directed
+        Acyclic Word Graph (DAWG).
+        The order in which nodes are merged is determined by the
+        maxdepth to leaves, which leads to an optimally compressed
+        structure.
+        """
+        depthdict = defaultdict(list)
+        for n in G.nodes:
+            depthdict[n.maxdepth] += [n]
+        for d in sorted(depthdict.keys()):
+            hashdict = defaultdict(list)
+            for n in depthdict[d]:
+                hashdict[hash(n)] += [n]
+            for h in hashdict:
+                nodes = hashdict[h]
+                receiver = nodes[0]
+                for n in nodes[1:]:  # The parents adopt the receiver
+                    n.parent.children[n.parentlabel] = receiver 
+                    del n
+
 
 if __name__ == "__main__":
     lex = "car cars cat cats do dog dogs done ear ears eat eats"
-    G = Graph()
+    G = DirectedGraph()
     G.parselex(lex)
+    trie_to_dawg(G)
 
